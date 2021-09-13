175d1f2c9d03cfeb128dbaefd7599ebd55e853dd
Rodrigo de França <howdrigo.fr@gmail.com>
2021-09-13 10:57:02 -0300

    � first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
